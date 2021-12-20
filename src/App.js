a9cbe3543f5fde3259e202fc68a97c1da73b28b2
DatexPro <82541977+DatexPro@users.noreply.github.com>
2021-12-21 00:43:18 +0200

    ДЗ9-10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import store from "./store";
+import Button from '@material-ui/core/Button';
+import React from "react";
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            out: '0'
+        }
+        this.refOutput = React.createRef()
+    }
+    tapeNumber(value){
+        let correntValue = value
+        let output = this.refOutput.current
+        this.setState({
+            out: correntValue
+        })
+        if (output.value==='0'){output.value=''}
+        output.value += correntValue
+    }
+    tapeOperation(value){
+        let output = this.refOutput.current
+        if(value === 'CE'){
+            output.value.length===1 ? output.value='0' : output.value=output.value.substring(0,output.value.length-1)
+        }
+        else if (value === 'C'){output.value='0'}
+        else if (value ==='='){
+            try{output.value=eval(output.value)
+                if (output.value==='Infinity')
+                {setTimeout(()=>{
+                    output.value = '0'
+                }, 1000)}
+            }
+            catch {
+                output.value='Не допустимое значение'
+                setTimeout(()=>{
+                    output.value = '0'
+                }, 1000)
+            }
+        }
+
+    }
+    render() {
+        return (
+            <div className={"container"}>
+                <div className={"output"}>
+                    <input ref={this.refOutput} type={"text"} defaultValue={this.state.out}/>
+                </div>
+                <div className={"button"}>
+                    {store.button.map((item,index)=> <Button
+                        key={index}
+                        onClick={() => {this.tapeNumber(item.val)}}>
+                        {item.val}</Button>)}
+                    {store.operation.map((item,index)=><Button
+                        key={index}
+                        onClick={() => {this.tapeOperation(item.val)}}>
+                        {item.val}
+                    </Button>)}
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,10 @@
+let store = {
+    button:[
+        {val:'1'},{val:'2'},{val:'3'},{val:'4'},{val:'5'},{val:'6'},{val:'7'},{val:'8'},{val:'9'},{val:'0'},
+        {val:'+'},{val:'-'},{val:'*'},{val:'/'}
+    ],
+    operation:[
+        {val:'='},{val:'C'},{val:'CE'}
+    ]
+}
+export default store
